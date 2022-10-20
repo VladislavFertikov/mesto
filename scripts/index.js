5174dd05d73c57ad42ed9f5e1358711389f44b55
Vladislav Fertikov <fertikov.vladislav@yandex.ru>
2022-10-20 21:08:34 +0400

    mesto2

--- scripts/index.js
+++ scripts/index.js
@@ -5,17 +5,18 @@ const popupOpenButtonElement = document.querySelector(".profile__edit-button");
 const openPopup = function () {
   popupElement.classList.add("popup_is-opened");
 };
+popupOpenButtonElement.addEventListener("click", openPopup);
 
 const closePopup = function () {
   popupElement.classList.remove("popup_is-opened");
+  nameInput.value = nameTitle.textContent;
+  jobInput.value = profileSubtitle.textContent;
 };
-
-popupOpenButtonElement.addEventListener("click", openPopup);
 popupCloseButtonElement.addEventListener("click", closePopup);
 
 let formElement = popupElement.querySelector(".popup__content");
-let nameInput = formElement.querySelector(".popup__input_name");
-let jobInput = formElement.querySelector(".popup__input_profession");
+let nameInput = formElement.querySelector(".popup__input_type_name");
+let jobInput = formElement.querySelector(".popup__input_type_profession");
 let nameTitle = document.querySelector(".profile__title");
 let profileSubtitle = document.querySelector(".profile__subtitle");
 let popupSaveButton = popupElement.querySelector(".popup__save");
@@ -24,7 +25,7 @@ function formSubmitHandler(evt) {
   evt.preventDefault();
   nameTitle.textContent = nameInput.value;
   profileSubtitle.textContent = jobInput.value;
+  closePopup();
 }
+
 formElement.addEventListener("submit", formSubmitHandler);
-popupSaveButton.addEventListener("click", formSubmitHandler);
-popupSaveButton.addEventListener("click", closePopup);
